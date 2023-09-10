0daf9d11f2dd00e6e47983ba067dd7c4f4ce4bcf
Dhani <mramdhani010@gmail.com>
2023-09-10 16:24:40 +0700

    init

---
+++ string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad Ramdhani")
+	fmt.Println("jumlah karakter =", len("Muhammad Ramdhani"))
+	fmt.Println("karakter ke 1 =", "Muhammad Ramdhani"[0]) // balikannya representasi dari byte
+}
