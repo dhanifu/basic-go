0b4d71d2ef840ada7d5a7b4a26e2ec0f1616996b
Muhammad Ramdhani <mramdhani010@gmail.com>
2023-10-02 14:15:25 +0700

    update function return named new

--- function-return-named-value.go
+++ function-return-named-value.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func getFullName2() (firstName string, middleName string, lastName string) {
-	firstName = "Eko"
-	middleName = "Kurniawan"
-	lastName = "Khannedy"
+	firstName = "Muhammad"
+	middleName = "Ramdhani"
+	lastName = "Gaada"
 
 	return
+}
+
+func getIdentity() (name string, age int, married bool) {
+	name = "Muhammad Ramdhani"
+	age = 20
+	married = false
+
+	return 
 }
 
 func main() {
@@ -15,4 +23,10 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+
+	name, age, married := getIdentity()
+
+	fmt.Println("name    :",name)
+	fmt.Println("age     :",age)
+	fmt.Println("married :",married)
 }
